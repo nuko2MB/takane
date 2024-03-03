249a8a2b37ab03f5f68919664bbd84368f6935a2
nuko <git@nuko.boo>
2024-03-03 07:15:57 -0700

    update swayidle

--- modules/home/services/swayidle/default.nix
+++ modules/home/services/swayidle/default.nix
@@ -22,11 +22,13 @@ lib.nuko.mkModule args
             timeout = 600;
             command = "${swaylock}/bin/swaylock";
           }
-          {
-            timeout = 900;
-            command = "${hyprctl} dispatch dpms off";
-            resumeCommand = "${hyprctl} dispatch dpms on";
-          }
+          # Unstable Regression: 'DPMS on' after resume is not currently working.
+          /* {
+                      timeout = 900;
+                      command = "${hyprctl} dispatch dpms off";
+                      resumeCommand = "${hyprctl} dispatch dpms on";
+                    }
+          */
           {
             timeout = 1200;
             command = "${lib.getExe' pkgs.systemd "systemctl"} suspend";
@@ -34,8 +36,18 @@ lib.nuko.mkModule args
         ];
         events = [
           {
+            # When manually suspending the system it should lock instantly.
             event = "before-sleep";
-            command = "${swaylock}/bin/swaylock";
+            command = "${swaylock}/bin/swaylock --grace 0";
+          }
+          {
+            # When manually locking the system it should lock instantly.
+            event = "lock";
+            command = "${swaylock}/bin/swaylock --grace 0";
+          }
+          {
+            event = "after-resume";
+            command = "${hyprctl} dispatch dpms on";
           }
         ];
         systemdTarget = "hyprland-session.target";
